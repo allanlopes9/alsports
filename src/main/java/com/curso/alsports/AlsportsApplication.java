ccba2f249acf7f4c3c858b864dc71e379a7981d2
Allan Lopes Albino <allanlopes0100@gmail.com>
2026-08-20 19:42:56 -0300

    feat: cria projeto inicial da AL Sports

---
+++ src/main/java/com/curso/alsports/AlsportsApplication.java
@@ -0,0 +1,13 @@
+package com.curso.alsports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlsportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlsportsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/alsports/AlsportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.alsports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlsportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
