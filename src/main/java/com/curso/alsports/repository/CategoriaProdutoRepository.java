08e470b54c37bc465188adad9ec9cd1d45cbc4a0
Allan Lopes Albino <allanlopes0100@gmail.com>
2026-08-21 18:49:49 -0300

    feat: configura acesso aos dados com JPA

---
+++ src/main/java/com/curso/alsports/repository/CategoriaProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.curso.alsports.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.curso.alsports.model.CategoriaProduto;
+
+public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProduto, Long> {
+}
