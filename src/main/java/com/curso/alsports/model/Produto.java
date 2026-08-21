665bd2e314d401b9c0cbb053dd325ada7f3c4a0e
Allan Lopes Albino <allanlopes0100@gmail.com>
2026-08-21 12:50:47 -0300

    feat: configura entidades JPA

--- src/main/java/com/curso/alsports/model/Produto.java
+++ src/main/java/com/curso/alsports/model/Produto.java
@@ -3,14 +3,26 @@ package com.curso.alsports.model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
     private Integer quantidade;
     private BigDecimal preco;
     private LocalDate dataCadastro;
     private Boolean ativo;
+
+    @ManyToOne
     private CategoriaProduto categoria;
 
     public Long getId() {
