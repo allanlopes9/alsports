c4f100757f7f688cb069523d0f140f0308c1c385
Allan Lopes Albino <allanlopes0100@gmail.com>
2026-08-21 00:26:33 -0300

    feat: cria entidade Produto

---
+++ src/main/java/com/curso/alsports/model/Produto.java
@@ -0,0 +1,71 @@
+package com.curso.alsports.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Produto {
+
+    private Long id;
+    private String nome;
+    private Integer quantidade;
+    private BigDecimal preco;
+    private LocalDate dataCadastro;
+    private Boolean ativo;
+    private CategoriaProduto categoria;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public LocalDate getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(LocalDate dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public Boolean getAtivo() {
+        return ativo;
+    }
+
+    public void setAtivo(Boolean ativo) {
+        this.ativo = ativo;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(CategoriaProduto categoria) {
+        this.categoria = categoria;
+    }
+}
